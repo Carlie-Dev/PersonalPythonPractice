ae28bb1bf8b5b817bdab9fc019d99491a5e47f33
Carlie-Dev <128517876+CarliePeters@users.noreply.github.com>
2024-01-23 14:53:58 -0600

    numbers and hello world

--- personal.py
+++ personal.py
@@ -1,3 +1,6 @@
+#print
+print('Hello Word!')
+
 #how to take an input. input() reads the input and sets the variable to whatever is typed in it's place.
 print('Input your last name:')
 last_name = input()
@@ -5,3 +8,14 @@ last_name = input()
 print('input your first name:')
 first_name = input()
 print(last_name, '-', first_name)
+
+#Getting numbers
+print('Enter a number:')
+num1 = int(input())
+print('Enter a number:')
+num2 = int(input())
+print('Enter a number:')
+num3 = int(input())
+print('The product of the numbers you entered is:')
+print(num1*num2*num3)
+
