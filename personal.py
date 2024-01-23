0781d7b900ed47c8acdebf95d5cd0a43ceb9ba5d
Carlie-Dev <128517876+CarliePeters@users.noreply.github.com>
2024-01-23 14:45:53 -0600

    Getting inputs

---
+++ personal.py
@@ -0,0 +1,7 @@
+#how to take an input. input() reads the input and sets the variable to whatever is typed in it's place.
+print('Input your last name:')
+last_name = input()
+
+print('input your first name:')
+first_name = input()
+print(last_name, '-', first_name)
