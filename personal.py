c6e6342ceb0b180509488e6ec511fdf0ae8951ed
Carlie-Dev <128517876+CarliePeters@users.noreply.github.com>
2024-02-02 09:43:10 -0600

    more references from class

--- personal.py
+++ personal.py
@@ -25,3 +25,85 @@ num_condos = input()
 
 print('Sam sold', num_condos, 'condos.')
 
+#Length of name
+name = 'Carlie'
+print(len(name))
+print(name[2])
+
+num1 = 14924
+num2 = 140002
+num3 = 247
+num4 = 203
+
+print(f'Hello, {name}, nice to meet you!')
+
+#Displaying number with commas
+print(f'{num1:,d}')
+
+#displaying number with leading zero
+print(f'{num2:07d}')
+
+#displaying number in binary
+print(f'{num3:b}')
+
+#Hexidecimal
+print(f'{num4:x}')
+
+#Capital Hexidecimal
+print(f'{num4:X}')
+
+red = 144
+green = 39
+blue = 207
+
+print(f'Red Binary: {red:b} | Red Hexidecimal: {red:x}')
+print(f'Green Binary: {green:b} | Green Hexidecimal: {green:x}')
+print(f'Blue Binary: {blue:b} | Blue Hexidecimal: {blue:x}')
+
+#lists and collections
+myList = [red,green,blue,255,'singer']
+print(myList[4])
+myList.pop(3)
+print(myList[3])
+
+myList.remove('singer')
+print(myList)
+
+#Strings and lists are types of collections so they behave similarly and both use len() for getting the length
+#Length of list
+print(len(myList))
+
+myList2 = [100,25,209]
+
+#Combining lists
+superList = myList + myList2
+print(superList)
+
+#lists of lists
+superList = [myList, myList2]
+print(superList)
+
+#opperations for lists
+print(sum(myList))
+print(max(superList))
+#The index of 39 is one
+print(myList.index(39))
+
+
+
+
+#Sets
+#Sets are similar because they are a collection
+#Unlike a list it can only contain unique data
+
+mySet = {30,11,59,600}
+#mySet.add(30) will not change it because 30 already exists
+print(mySet)
+#use the value to get the value
+print(mySet)
+
+
+#Dictionary
+myDict = {'Burger': 1.99, 'ChickenStrip': 2.99, 'Soda':.99}
+
+print(myDict['Burger'])
